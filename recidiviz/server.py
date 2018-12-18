457e0298144451860d86c52f9eaa5fc4d8779a0a
Zach Garcia <garciaz@google.com>
2018-12-18 16:38:01 -0500

    Move sqlalchemy engine creation to server.py

--- recidiviz/server.py
+++ recidiviz/server.py
@@ -18,12 +18,20 @@
 """Entrypoint for the application."""
 
 from flask import Flask
+import sqlalchemy
 
 from recidiviz.ingest.infer_release import infer_release_blueprint
 from recidiviz.ingest.scraper_control import scraper_control
 from recidiviz.ingest.worker import worker
+from recidiviz.persistence.database.schema import Base
 from recidiviz.tests.utils.populate_test_db import test_populator
 from recidiviz.utils import environment
+from recidiviz.utils import secrets
+
+
+# SQLAlchemy engine string prefix declaring the database type and driver.
+_ENGINE_PREFIX = 'mysql+pymysql'
+
 
 app = Flask(__name__)
 app.register_blueprint(scraper_control, url_prefix='/scraper')
@@ -31,3 +39,21 @@ app.register_blueprint(worker, url_prefix='/scraper')
 app.register_blueprint(infer_release_blueprint, url_prefix='/infer_release')
 if not environment.in_prod():
     app.register_blueprint(test_populator, url_prefix='/test_populator')
+
+db_user = secrets.get_secret('sqlalchemy_db_user')
+db_password = secrets.get_secret('sqlalchemy_db_password')
+db_host = secrets.get_secret('sqlalchemy_db_host')
+db_name = secrets.get_secret('sqlalchemy_db_name')
+db_connection_name = secrets.get_secret('sqlalchemy_db_connection_name')
+
+sqlalchemy_url = '{engine_prefix}://{db_user}:{db_password}@{db_host}/'\
+                 '{db_name}?unix_socket={db_connection_name}'.format(
+                     engine_prefix=_ENGINE_PREFIX,
+                     db_user=db_user,
+                     db_password=db_password,
+                     db_host=db_host,
+                     db_name=db_name,
+                     db_connection_name=db_connection_name)
+engine = sqlalchemy.create_engine(sqlalchemy_url)
+Base.metadata.create_all(engine)
+Session.configure(bind=engine)
