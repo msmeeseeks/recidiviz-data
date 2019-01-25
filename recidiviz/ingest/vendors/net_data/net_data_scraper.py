400638a8f003a431a1a034858ec4ea1939814d1b
terinpw <terinpw@google.com>
2019-01-25 14:19:49 -0500

    Create Net Data Vendor Scraper (+ 2 regions) (#723)
    * functioning us_tx_brown scraper, needs tests
    
    * update populate_data to use ScrapedData object
    
    * clean net_data scraper a bit
    
    * tests passing
    
    * added us_tx_cooke
    
    * refactored tests to have centralized NetData tests
    
    * fixed all tests
    
    * fix tests
    
    * removed ustxbrown specific fixtures
    
    * addressed pr comments and fixed lint errors:
    
    * removed necessity for initial_endpoint
    
    * addressed PR comments

---
+++ recidiviz/ingest/vendors/net_data/net_data_scraper.py
@@ -0,0 +1,225 @@
+# Recidiviz - a platform for tracking granular recidivism metrics in real time
+# Copyright (C) 2018 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Generic scraper implementation for all regions using NET Data vendor.
+
+Scraper flow:
+    1. With main html, scrapes required post data necessary to reach roster.
+    2. With roster xml, requests roster xsl stylesheet.
+    3. With roster xsl, merges roster xml and xsl into html. Scrapes necessary
+       post data for individual people and scrapes action needed to go to
+       person pages. Queues tasks for each person.
+    4. With person xml, requests person xsl stylesheet.
+    5. (In populate_data): With person xsl, merges person xml and xsl into html.
+       Uses html data extractor to extract all key/value pairs for this person,
+       then manually merges any found bookings that share the same admission and
+       release date. Finally adds the found booking_id onto the scraped open
+       booking (booking_id is provided only for the current booking).
+"""
+
+import logging
+import os
+import re
+from copy import copy
+from typing import Optional, List
+from urllib.parse import urlparse
+
+import more_itertools
+from lxml import etree, html
+
+from recidiviz.ingest import constants
+from recidiviz.ingest.base_scraper import BaseScraper
+from recidiviz.ingest.errors import ScraperError
+from recidiviz.ingest.extractor.html_data_extractor import HtmlDataExtractor
+from recidiviz.ingest.models.ingest_info import IngestInfo, _Booking
+from recidiviz.ingest.task_params import Task, ScrapedData
+
+_BOOKING_ID_KEY = 'ARREST NO'
+_FORM_KEY = 'S100KEY'
+_PAGE_TYPE_KEY = 'page_type'
+_PAGE_TYPE_ROSTER = 'roster'
+_PAGE_TYPE_ROSTER_XSL = 'roster_xsl'
+_PAGE_TYPE_PERSON = 'person'
+_PATH_PREFIX = 'bok/'
+_PERSON_KEY_SUBSTR = 'Javascript:run100'
+_PERSON_KEY_REGEX = r'Javascript:.*\'(.*?)\''
+_PERSON_PAGE_CONTENT_KEY = 'person_page_content'
+_PERSON_FORM_XPATH = '//form[@id="S100Form"]'
+_POST_DATA_XPATH = '//input[@type="hidden"]'
+_ROSTER_CONTENT_KEY = 'roster_content'
+_ROSTER_FORM_XPATH = '//form[@id="form1"]'
+_STYLESHEET_REGEX = r'href=\"(.*?)\"'
+
+
+class NetDataScraper(BaseScraper):
+    """Scraper for counties using NET Data Scraper."""
+
+    def __init__(self, region_name, mapping_filepath=None):
+        if not mapping_filepath:
+            mapping_filepath = os.path.join(
+                os.path.dirname(__file__), 'net_data.yaml')
+        self.mapping_filepath = mapping_filepath
+        super(NetDataScraper, self).__init__(region_name)
+
+    def get_more_tasks(self, content, task: Task) -> List[Task]:
+        if self.is_initial_task(task.task_type):
+            return _get_roster_task(content, task)
+
+        if self.should_get_more_tasks(task.task_type):
+            if task.custom[_PAGE_TYPE_KEY] == _PAGE_TYPE_ROSTER:
+                return _get_roster_stylesheet_task(content, task)
+            if task.custom[_PAGE_TYPE_KEY] == _PAGE_TYPE_ROSTER_XSL:
+                return _get_person_tasks(content, task)
+            if task.custom[_PAGE_TYPE_KEY] == _PAGE_TYPE_PERSON:
+                return _get_person_stylesheet_task(content, task)
+        raise ScraperError('Unexpected task configuration %s' % task)
+
+    def populate_data(
+            self, content, task: Task,
+            ingest_info: IngestInfo) -> Optional[ScrapedData]:
+        html_content = _get_html_content(
+            task.custom[_PERSON_PAGE_CONTENT_KEY], content)
+        data_extractor = HtmlDataExtractor(self.mapping_filepath)
+        ingest_info = data_extractor.extract_and_populate_data(
+            html_content, ingest_info)
+
+        open_booking_id = data_extractor.get_value(_BOOKING_ID_KEY)
+        _merge_bookings(ingest_info)
+        _put_booking_id_on_open_booking(
+            ingest_info, open_booking_id, self.region.region_code)
+        return ScrapedData(ingest_info=ingest_info)
+
+
+def _get_roster_task(content, task: Task) -> List[Task]:
+    path = _PATH_PREFIX + more_itertools.one(
+        content.xpath(_ROSTER_FORM_XPATH)).action
+    endpoint = urlparse(task.endpoint)._replace(path=path).geturl()
+    post_data = {elem.name: elem.value for elem in
+                 content.xpath(_POST_DATA_XPATH)}
+    task = Task(endpoint=endpoint, post_data=post_data,
+                task_type=constants.TaskType.GET_MORE_TASKS,
+                response_type=constants.ResponseType.TEXT,
+                custom={_PAGE_TYPE_KEY: _PAGE_TYPE_ROSTER})
+    return [task]
+
+
+def _get_roster_stylesheet_task(content, task: Task) -> List[Task]:
+    path = _PATH_PREFIX + _get_stylesheet(content)
+    endpoint = urlparse(task.endpoint)._replace(path=path).geturl()
+    task = Task(endpoint=endpoint,
+                task_type=constants.TaskType.GET_MORE_TASKS,
+                response_type=constants.ResponseType.RAW,
+                custom={_ROSTER_CONTENT_KEY: content,
+                        _PAGE_TYPE_KEY: _PAGE_TYPE_ROSTER_XSL})
+    return [task]
+
+
+def _get_person_tasks(content, task: Task) -> List[Task]:
+    html_content = _get_html_content(task.custom[_ROSTER_CONTENT_KEY],
+                                     content)
+    post_data = {elem.name: elem.value for elem in
+                 html_content.xpath(_POST_DATA_XPATH)}
+    path = _PATH_PREFIX + more_itertools.one(
+        html_content.xpath(_PERSON_FORM_XPATH)).action
+    new_url = urlparse(task.endpoint)._replace(path=path).geturl()
+
+    task_list = []
+    for key in _get_person_keys(html_content):
+        post_data_for_key = copy(post_data)
+        post_data_for_key[_FORM_KEY] = key
+        task_list.append(
+            Task(endpoint=new_url,
+                 post_data=post_data_for_key,
+                 task_type=constants.TaskType.GET_MORE_TASKS,
+                 response_type=constants.ResponseType.TEXT,
+                 custom={_PAGE_TYPE_KEY: _PAGE_TYPE_PERSON}))
+    return task_list
+
+
+def _get_stylesheet(raw_content) -> str:
+    search_result = re.search(_STYLESHEET_REGEX, raw_content)
+    if not search_result:
+        raise ScraperError('No stylesheet found in raw_content')
+    return search_result.group(1)
+
+
+def _get_person_stylesheet_task(content, task: Task) -> List[Task]:
+    path = _PATH_PREFIX + _get_stylesheet(content)
+    endpoint = urlparse(task.endpoint)._replace(path=path).geturl()
+    task = Task(endpoint=endpoint, task_type=constants.TaskType.SCRAPE_DATA,
+                response_type=constants.ResponseType.RAW,
+                custom={_PERSON_PAGE_CONTENT_KEY: content})
+    return [task]
+
+
+def _get_person_keys(content) -> List[str]:
+    keys: List[str] = []
+    for link in content.xpath('//a/@href'):
+        if _PERSON_KEY_SUBSTR in link:
+            keys.append(more_itertools.one(
+                re.findall(_PERSON_KEY_REGEX, str(link))))
+    return keys
+
+
+def _get_html_content(xml_content, stylesheet_content):
+    xslt_tree = etree.XML(stylesheet_content)
+    xml_elems = etree.XML(xml_content).getroottree()
+    return html.fromstring(str(xml_elems.xslt(xslt_tree)))
+
+
+def _merge_bookings(ingest_info: IngestInfo) -> None:
+    """By default, the Data Extractor will create a single booking for each
+    scraped charge. This method compresses all bookings that share the same
+    admission and release date."""
+
+    for person in ingest_info.people:
+        merged_bookings: List[_Booking] = []
+        for booking in person.bookings:
+            merged_booking = _find_matching_booking(
+                booking, merged_bookings)
+            if merged_booking:
+                merged_booking.charges.extend(booking.charges)
+            else:
+                merged_bookings.append(booking)
+        person.bookings = merged_bookings
+
+
+def _find_matching_booking(
+        booking_to_match: _Booking, bookings: List[_Booking]) \
+        -> Optional[_Booking]:
+    for booking in bookings:
+        if booking.admission_date == booking_to_match.admission_date and \
+                booking.release_date == booking_to_match.release_date:
+            return booking
+    return None
+
+
+def _put_booking_id_on_open_booking(
+        ingest_info: IngestInfo,
+        open_booking_id: Optional[str], region_code: str) -> None:
+    if not open_booking_id:
+        return
+
+    open_booking_exists = False
+    for booking in more_itertools.one(ingest_info.people).bookings:
+        if not booking.release_date:
+            open_booking_exists = True
+            booking.booking_id = open_booking_id
+
+    if not open_booking_exists:
+        logging.error(
+            'Scraped open booking id: %s, but no open booking found on '
+            'the scraped person for region %s', open_booking_id, region_code)
