3f305aa2e58ca116f6022112f3c2f7ae6817b920
Colin Adams <colinadams@google.com>
2019-02-27 17:28:09 -0500

    meow

---
+++ recidiviz/ingest/scrape/scraper_status.py
@@ -0,0 +1,66 @@
+# Recidiviz - a platform for tracking granular recidivism metrics in real time
+# Copyright (C) 2018 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ============================================================================
+"""Checks the status of scrapers to detect when they have completed."""
+
+from http import HTTPStatus
+import logging
+import threading
+
+from flask import Blueprint, request
+
+from recidiviz.ingest.scrape import ingest_utils, queues
+from recidiviz.utils import regions
+from recidiviz.utils.auth import authenticate_request
+from recidiviz.utils.params import get_values
+
+scraper_status = Blueprint('scraper_status', __name__)
+
+@scraper_status.route('/check_finished')
+@authenticate_request
+def check_for_finished_scrapers():
+    """Checks for any finished scrapers and kicks off next processes.
+    """
+
+    def _check_finished(region_code: str):
+        if is_scraper_finished(region_code):
+            # TODO: close the session, launch batch write
+            logging.info('Region \'%s\' has completed.', region_code)
+
+    region_codes = ingest_utils.validate_regions(
+        get_values('region', request.args))
+
+    threads = []
+    for region_code in region_codes:
+        # If timezone wasn't provided, return all but if it was, only
+        # return regions that match the timezone.
+        thread = threading.Thread(
+            target=_check_finished,
+            args=region_code
+        )
+        thread.start()
+        threads.append(thread)
+
+    # Once everything is started, lets wait for everything to end before
+    # returning.
+    for thread in threads:
+        thread.join()
+    return ('', HTTPStatus.OK)
+
+def is_scraper_finished(region_code: str):
+    region = regions.get_region(region_code)
+    return len(queues.list_tasks(region_code=region_code,
+                                 queue_name=region.get_queue_name())) == 0
