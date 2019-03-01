c4241993c1282a94ccc8ed34d3e695ec48a72232
Oliver Hinds <ohinds@gmail.com>
2019-03-01 14:56:26 -0500

    Fix lint issues.

--- recidiviz/ingest/aggregate/regions/tn/tn_aggregate_ingest.py
+++ recidiviz/ingest/aggregate/regions/tn/tn_aggregate_ingest.py
@@ -1,9 +1,27 @@
+# Recidiviz - a platform for tracking granular recidivism metrics in real time
+# Copyright (C) 2019 Recidiviz, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+"""Ingest TN aggregate jail data.
+"""
+import os
+from typing import Dict
+
 import datetime
 import dateparser
-import numpy as np
-import os
 import pandas as pd
-import sys
 import tabula
 import us
 
@@ -18,13 +36,13 @@ _MANUAL_FACILITY_TO_COUNTY_MAP = {
 }
 
 
-def parse(filename):
+def parse(filename: str) -> Dict:
 
     table = tabula.read_pdf(filename, pages=[2, 3, 4], multiple_tables=True)
 
     formatted_dfs = []
     for df in table:
-        formatted_dfs.append(format_table(df))
+        formatted_dfs.append(_format_table(df))
 
     table = pd.concat(formatted_dfs, ignore_index=True)
 
@@ -53,7 +71,8 @@ def _parse_date(filename: str) -> datetime.date:
     return aggregate_ingest_utils.on_last_day_of_month(d)
 
 
-def format_table(df):
+def _format_table(df: pd.DataFrame) -> pd.DataFrame:
+    """Format the dataframe that comes from one page of the PDF."""
 
     # The first four rows are parsed containing the column names.
     df.columns = df.iloc[:4].apply(lambda rows: ' '.join(rows.dropna()).strip())
@@ -86,7 +105,7 @@ def format_table(df):
     return df
 
 
-def _pretend_facility_is_county(facility_name: str):
+def _pretend_facility_is_county(facility_name: str) -> str:
     """Format facility_name like a county_name to match each to a fips."""
     if facility_name in _MANUAL_FACILITY_TO_COUNTY_MAP:
         return _MANUAL_FACILITY_TO_COUNTY_MAP[facility_name]
